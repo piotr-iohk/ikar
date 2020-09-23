c373205b9ca69f2441071c207c6af31e06975c85
Piotr Stachyra <piotr.stachyra@iohk.io>
2020-09-23 16:15:03 +0200

    fix metadata example

--- helpers/app_helpers.rb
+++ helpers/app_helpers.rb
@@ -105,13 +105,50 @@ module Helpers
             <details>
               <summary>Examplary metadata</summary>
                 <code>
-                   {
-                    "0": "cardano",
-                    "1": 14,
-                    "2": "0x2512a00e9653fe49a44a5886202e24d77eeb998f",
-                    "3": [1,2,4],
-                    "4": [["k1", "v1"], ["k2", "v2"]]
-                    }
+                {
+                   "0":{
+                      "string":"cardano"
+                   },
+                   "1":{
+                      "int":14
+                   },
+                   "2":{
+                      "bytes":"2512a00e9653fe49a44a5886202e24d77eeb998f"
+                   },
+                   "3":{
+                      "list":[
+                         {
+                            "int":14
+                         },
+                         {
+                            "int":42
+                         },
+                         {
+                            "string":"1337"
+                         }
+                      ]
+                   },
+                   "4":{
+                      "map":[
+                         {
+                            "k":{
+                               "string":"key"
+                            },
+                            "v":{
+                               "string":"value"
+                            }
+                         },
+                         {
+                            "k":{
+                               "int":14
+                            },
+                            "v":{
+                               "int":42
+                            }
+                         }
+                      ]
+                   }
+                }
                 </code>
             </details>
           </small>
